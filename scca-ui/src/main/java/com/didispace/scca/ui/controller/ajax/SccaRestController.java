f9cd27388e5512306e6f5502b5f03c8b9ea69ed7
zhaiyongchao <dyc87112@qq.com>
2018-06-16 17:35:42 +0800

    bug修复 + 简单重构

--- scca-ui/src/main/java/com/didispace/scca/ui/controller/ajax/SccaRestController.java
+++ scca-ui/src/main/java/com/didispace/scca/ui/controller/ajax/SccaRestController.java
@@ -1,7 +1,6 @@
 package com.didispace.scca.ui.controller.ajax;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
@@ -10,13 +9,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.client.RestTemplate;
-import sun.net.www.http.HttpClient;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.net.URI;
-import java.util.Objects;
 
 /**
  * Created by stone-jin on 2018/5/28.
@@ -43,36 +40,44 @@ public class SccaRestController {
             log.info("forwardUrl: {}", originalUrl);
             forwardUrl = originalUrl;
 
-            URI uri;
-
-            uri = new URI("http://127.0.0.1:10030" + forwardUrl);
+            URI uri = new URI("http://127.0.0.1:10030" + forwardUrl);
 
             forwardUrl = uri.toString();
             // 日志输出根据具体项目而定
             log.info("{}, action=REQUEST, queryString={}, forwardUrl={}, requestBody={}",
                     originalUrl, queryString, uri, requestBody);
+
+            // 头信息统一处理
+            HttpHeaders headers = new HttpHeaders();
+            headers.add(HttpHeaders.CONTENT_TYPE, "application/json;charset=utf-8");
             RestTemplate restTemplate = new RestTemplate();
 
-            if (Objects.equals(request.getMethod(), "GET")) {
-                String url = "";
-                if (Objects.equals(null, queryString)) {
-                    url = uri.toString();
-                } else {
-                    url = uri.toString() + "?" + queryString;
-                }
-                result = restTemplate.getForObject(url, String.class);
-            } else if (Objects.equals(request.getMethod(), "POST")) {
-                // FIXME http 415
-                HttpHeaders headers = new HttpHeaders();
-                headers.add(HttpHeaders.CONTENT_TYPE, "application/json");
-                HttpEntity<String> requestEntity = new HttpEntity<>(requestBody, headers);
-                ResponseEntity<String> responseEntity = restTemplate.exchange(uri.toString(), HttpMethod.POST, requestEntity, String.class);
-                result = responseEntity.getBody();
-//                result = restTemplate.postForObject(uri.toString(), requestBody, String.class);
-            } else if (Objects.equals(request.getMethod(), "DELETE")) {
-                restTemplate.delete(uri.toString());
-            } else if (Objects.equals(request.getMethod(), "PUT")) {
-                restTemplate.put(uri.toString(), requestBody);
+            // url统一处理
+            String url = uri.toString();
+            if(null != queryString) {
+                url = url + "?" + queryString;
+            }
+
+            HttpEntity<String> requestEntity;
+            ResponseEntity<String> responseEntity;
+
+            switch (request.getMethod()) {
+                case "GET" :
+                    result = restTemplate.getForObject(url, String.class);
+                    break;
+                case "DELETE":
+                    restTemplate.delete(url);
+                    break;
+                case "POST":
+                    requestEntity = new HttpEntity<>(requestBody, headers);
+                    responseEntity = restTemplate.exchange(url, HttpMethod.POST, requestEntity, String.class);
+                    result = responseEntity.getBody();
+                    break;
+                case "PUT":
+                    requestEntity = new HttpEntity<>(requestBody, headers);
+                    responseEntity = restTemplate.exchange(url, HttpMethod.PUT, requestEntity, String.class);
+                    result = responseEntity.getBody();
+                    break;
             }
 
             //  日志输出根据具体项目而定
