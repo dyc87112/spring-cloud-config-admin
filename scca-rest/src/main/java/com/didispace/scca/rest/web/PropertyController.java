0eb3e254bafc8581ba935a35605f5d3ebef47595
zhaiyongchao <dyc87112@qq.com>
2018-06-21 17:47:20 +0800

    实现加密/解密接口，实现保存配置接口

--- scca-rest/src/main/java/com/didispace/scca/rest/web/PropertyController.java
+++ scca-rest/src/main/java/com/didispace/scca/rest/web/PropertyController.java
@@ -1,15 +1,13 @@
 package com.didispace.scca.rest.web;
 
+import com.didispace.scca.core.domain.Env;
 import com.didispace.scca.rest.dto.base.WebResp;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.config.environment.Environment;
 import org.springframework.cloud.config.environment.PropertySource;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Properties;
@@ -26,25 +24,57 @@ import java.util.Properties;
 @RequestMapping("/property")
 public class PropertyController extends BaseController {
 
-    @ApiOperation("Get properties from persistent / 获取持久化的配置信息")
+    @ApiOperation("persistent properties / 获取持久化的配置信息")
     @RequestMapping(path = "/persistent", method = RequestMethod.GET)
-    public WebResp<Properties> propertiesFromPersistent(@RequestParam String project,
-                                                        @RequestParam String profile,
-                                                        @RequestParam String label) {
+    public WebResp<Properties> propertiesFromPersistent(@RequestParam("project") String project,
+                                                        @RequestParam("profile") String profile,
+                                                        @RequestParam("label") String label) {
         // 直接通过持久化实现，获取配置信息
         Properties properties = persistenceService.readProperties(project, profile, label);
         return WebResp.success(properties);
     }
 
-    @ApiOperation("Get properties from config server / 通过配置中心接口获取详细配置信息")
+    @ApiOperation("Save properties / 保存持久化的配置信息")
+    @RequestMapping(path = "/persistent", method = RequestMethod.POST)
+    public WebResp<String> saveProperties(@RequestParam("project") String project,
+                                          @RequestParam("profile") String profile,
+                                          @RequestParam("label") String label,
+                                          @RequestBody Properties newProperties) {
+        // 保存配置信息
+        persistenceService.saveProperties(project, profile, label, newProperties);
+        return WebResp.success("save properties success");
+    }
+
+    @ApiOperation("config server properties / 获取配置中心获取的配置信息")
     @RequestMapping(path = "/configServer", method = RequestMethod.GET)
-    public WebResp<List<PropertySource>> propertiesFromConfigServer(@RequestParam String project,
-                                                                    @RequestParam String profile,
-                                                                    @RequestParam String label) {
+    public WebResp<List<PropertySource>> propertiesFromConfigServer(@RequestParam("project") String project,
+                                                                    @RequestParam("profile") String profile,
+                                                                    @RequestParam("label") String label) {
         // 通过配置中心来获取配置信息, 参考应用获取的具体配置信息
         Environment environment = baseOptService.getProperties(project, profile, label);
         return WebResp.success(environment.getPropertySources());
     }
 
+    @ApiOperation("Encrypt property / 加密配置")
+    @RequestMapping(path = "/encrypt", method = RequestMethod.GET)
+    public WebResp<String> encrypt(@RequestParam("envId") Long envId,
+                                   @RequestParam("value") String value) {
+        // 指定某个环境的加密（单个值）
+        Env env = envRepo.findOne(envId);
+        String result = baseOptService.encrypt(value, env);
+        log.info("encrypt property {} -> {}", value, result);
+        return WebResp.success(result);
+    }
+
+    @ApiOperation("Decrypt property / 解密配置")
+    @RequestMapping(path = "/decrypt", method = RequestMethod.GET)
+    public WebResp<String> decrypt(@RequestParam("envId") Long envId,
+                                   @RequestParam("value") String value) {
+        // 指定某个环境的解密（单个值）
+        Env env = envRepo.findOne(envId);
+        String result = baseOptService.decrypt(value, env);
+        log.info("decrypt property {} -> {}", value, result);
+        return WebResp.success(result);
+    }
 
 }
