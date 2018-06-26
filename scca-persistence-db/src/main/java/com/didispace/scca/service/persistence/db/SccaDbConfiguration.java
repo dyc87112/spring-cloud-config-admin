f43b5e574ac878c8213133d43f0979496f63e3f9
程序猿DD <dyc87112@qq.com>
2018-06-26 10:52:25 +0800

    简化采用db存储时候的配置内容

--- scca-persistence-db/src/main/java/com/didispace/scca/service/persistence/db/SccaDbConfiguration.java
+++ scca-persistence-db/src/main/java/com/didispace/scca/service/persistence/db/SccaDbConfiguration.java
@@ -4,6 +4,7 @@ import com.didispace.scca.core.service.PersistenceService;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 /**
  * Created by 程序猿DD/翟永超 on 2018/5/2.
@@ -12,6 +13,7 @@ import org.springframework.context.annotation.Configuration;
  * Github: https://github.com/dyc87112/
  */
 @Configuration
+@PropertySource("scca-persistence-db.properties")
 public class SccaDbConfiguration {
 
     @Bean
