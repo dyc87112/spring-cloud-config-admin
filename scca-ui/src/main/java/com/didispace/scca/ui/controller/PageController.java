795351a51a635632ed6f0c90c397ba5abb2529c4
金炳 <hzjinbing@corp.netease.com>
2018-07-17 22:22:49 +0800

    fix #35: 增加根路径的重定向

--- scca-ui/src/main/java/com/didispace/scca/ui/controller/PageController.java
+++ scca-ui/src/main/java/com/didispace/scca/ui/controller/PageController.java
@@ -5,16 +5,24 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 /**
  * Created by stone-jin on 2018/5/28.
  */
 @Controller
-@RequestMapping("/admin")
+@RequestMapping("")
 public class PageController {
 
-    @GetMapping("")
+    @GetMapping("/admin")
     public String adminPage() {
         return "index";
+    }
+
+    @GetMapping("/")
+    public void indexPage(HttpServletResponse response) throws IOException {
+        response.sendRedirect("/admin");
     }
 
 }
