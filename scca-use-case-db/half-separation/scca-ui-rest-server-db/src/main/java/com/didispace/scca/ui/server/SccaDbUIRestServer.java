d7bfb6196d7124b647f0979103fb2bf56e4577c6
zhaiyongchao <dyc87112@qq.com>
2018-08-02 19:09:10 +0800

    案例整理：Git存储的全分离方案

---
+++ scca-use-case-db/half-separation/scca-ui-rest-server-db/src/main/java/com/didispace/scca/ui/server/SccaDbUIRestServer.java
@@ -0,0 +1,15 @@
+package com.didispace.scca.ui.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class SccaDbUIRestServer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SccaDbUIRestServer.class, args);
+    }
+
+}
---
+++ scca-use-case-git/full-separation/README.md
@@ -0,0 +1,6 @@
+## 全分离模式
+
+- scca-config-server-git: 采用db存储的配置中心，与半分离模式一样
+- scca-rest-server-git: scca的rest模块
+- scca-ui-server-git: scca的ui模块
+
