2b7474f0f119ff16e27450b302c222879c6bc9f6
zhaiyongchao <dyc87112@qq.com>
2018-08-02 19:00:15 +0800

    案例整理：Git存储的半分离方案

--- scca-use-case-git/half-separation/scca-ui-rest-server-git/src/main/java/com/didispace/scca/ui/server/SccaGitUIRestServer.java
+++ scca-use-case-git/half-separation/scca-ui-rest-server-git/src/main/java/com/didispace/scca/ui/server/SccaGitUIRestServer.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @EnableDiscoveryClient
 @SpringBootApplication
-public class SccaUIServerWithRestServer {
+public class SccaGitUIRestServer {
 
     public static void main(String[] args) {
-        SpringApplication.run(SccaUIServerWithRestServer.class, args);
+        SpringApplication.run(SccaGitUIRestServer.class, args);
     }
 
 }
---
+++ scca-use-case-git/half-separation/scca-ui-rest-server-git/src/test/java/com/didispace/scca/ui/SccaUiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.didispace.scca.ui;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SccaUiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
