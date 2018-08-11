5e4e8cf48dd196dcf0dccea0bde51e7166f7ba57
anoy <545544032@qq.com>
2018-08-11 16:28:18 +0800

    fix: delete with transaction

--- scca-rest/src/main/java/com/didispace/scca/rest/service/impl/UserServiceImpl.java
+++ scca-rest/src/main/java/com/didispace/scca/rest/service/impl/UserServiceImpl.java
@@ -9,6 +9,7 @@ import org.springframework.data.domain.Sort;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -43,6 +44,7 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
+    @Transactional
     public void deleteUserByUsername(String username) {
         userRepo.deleteByUsername(username);
     }
--- scca-rest/src/main/java/com/didispace/scca/rest/web/UserAdminController.java
+++ scca-rest/src/main/java/com/didispace/scca/rest/web/UserAdminController.java
@@ -21,7 +21,7 @@ import java.util.List;
 @Slf4j
 @RestController
 @RequestMapping("${scca.rest.context-path:}/admin")
-//@Secured("hasRole('ADMIN')")
+@Secured("ROLE_ADMIN")
 public class UserAdminController extends BaseController {
 
     @ApiOperation("Get User List / 获取用户列表")
