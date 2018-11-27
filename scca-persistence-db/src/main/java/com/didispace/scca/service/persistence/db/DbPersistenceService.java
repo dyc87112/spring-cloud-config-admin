661edefaffb29024abb50346a376fcfc20a0d923
y2w <yanyongwang@msn.com>
2018-11-27 18:48:58 +0800

    Update DbPersistenceService.java
    修复post时候的bug

--- scca-persistence-db/src/main/java/com/didispace/scca/service/persistence/db/DbPersistenceService.java
+++ scca-persistence-db/src/main/java/com/didispace/scca/service/persistence/db/DbPersistenceService.java
@@ -14,6 +14,7 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.Properties;
@@ -41,15 +42,15 @@ public class DbPersistenceService implements PersistenceService {
 	public Properties readProperties(String application, String profile, String label) {
 		String url = urlMakerService.configServerBaseUrl(profile)
 				+ "/readProperties?application={1}&profile={2}&label={3}";
-		Properties properties=request(url,HttpMethod.GET,Properties.class,application, profile, label);
-		return properties;
+
+		return getForObject(url, Properties.class, application, profile, label);
 	}
 
 	@Override
 	public void deletePropertiesByEnv(String profile) {
 		// 删除某个环境下的所有配置
 		String url = urlMakerService.configServerBaseUrl(profile) + "/deletePropertiesByEnv?profile={1}";
-		Integer rows =request(url,HttpMethod.GET,Integer.class,profile);
+		Integer rows = getForObject(url, Integer.class, profile);
 		log.info("delete env [{}] property rows {}", profile, rows);
 	}
 
@@ -59,7 +60,7 @@ public class DbPersistenceService implements PersistenceService {
 		for (Env env : envRepo.findAll()) {
 			String url = urlMakerService.configServerBaseUrl(env.getName())
 					+ "/deletePropertiesByProject?application={1}";
-			Integer rows =request(url,HttpMethod.GET,Integer.class,application);
+			Integer rows = getForObject(url, Integer.class, application);
 			log.info("delete project [{}] in env [{}] property rows {}", application, rows);
 		}
 	}
@@ -68,7 +69,7 @@ public class DbPersistenceService implements PersistenceService {
 	public void deletePropertiesByProjectAndEnv(String application, String profile) {
 		String url = urlMakerService.configServerBaseUrl(profile)
 				+ "/deletePropertiesByProjectAndEnv?application={1}&profile={2}";
-		Integer rows =request(url,HttpMethod.GET,Integer.class,profile);
+		Integer rows = getForObject(url, Integer.class, profile);
 		log.info("delete project [{}] in env [{}] property rows {}", application, profile, rows);
 	}
 
@@ -84,7 +85,7 @@ public class DbPersistenceService implements PersistenceService {
 	public void deletePropertiesByProjectAndEnvAndLabel(String application, String profile, String label) {
 		String url = urlMakerService.configServerBaseUrl(profile)
 				+ "/deletePropertiesByProjectAndEnvAndLabel?application={1}&profile={2}&label={3}";
-		Integer rows =request(url,HttpMethod.GET,Integer.class,application, profile, label);
+		Integer rows = getForObject(url, Integer.class, application, profile, label);
 		log.info("delete {}-{}-{} property rows {}", application, profile, label, rows);
 	}
 
@@ -92,23 +93,32 @@ public class DbPersistenceService implements PersistenceService {
 	public void saveProperties(String application, String profile, String label, Properties update) {
 		String url = urlMakerService.configServerBaseUrl(profile)
 				+ "/saveProperties?application={1}&profile={2}&label={3}";
-		Integer rows =request(url,HttpMethod.POST,Integer.class,application, profile, label);
+		Integer rows = postForObject(url, update, Integer.class, application, profile, label);
 		log.info("add {}-{}-{} rows {}", application, profile, label, rows);
 	}
 
 	@Override
 	public void updateProfileName(String oldName, String newName) {
 		String url = urlMakerService.configServerBaseUrl(oldName) + "/updateProfileName?oldName={1}&newName={2}";
-		Integer rows =request(url,HttpMethod.POST,Integer.class,oldName, newName);
+		Integer rows = postForObject(url, null, Integer.class, oldName, newName);
 		log.info("update profile name {} -> {}, change property rows {}", oldName, newName, rows);
 	}
 
-	private <T> T request(String url, HttpMethod method, Class<T> cls, Object... param) {
+	private <T> T getForObject(String url, Class<T> cls, Object... param) {
 		HttpHeaders headers = new HttpHeaders();
 		headers.add("Authorization",
 				Credentials.basic(sccaConfigServerProperties.getUsername(), sccaConfigServerProperties.getPassword()));
 		HttpEntity<T> entity = new HttpEntity<>(headers);
-		ResponseEntity<T> response = restTemplate.exchange(url, method, entity, cls, param);
+		ResponseEntity<T> response = restTemplate.exchange(url, HttpMethod.GET, entity, cls, param);
+		return response.getBody();
+	}
+
+	private <T> T postForObject(String url, Object request, Class<T> cls, Object... param) {
+		MultiValueMap<String, String> headers = new HttpHeaders();
+		headers.add("Authorization",
+				Credentials.basic(sccaConfigServerProperties.getUsername(), sccaConfigServerProperties.getPassword()));
+		HttpEntity<Object> entity = new HttpEntity<>(request, headers);
+		ResponseEntity<T> response = restTemplate.exchange(url, HttpMethod.POST, entity, cls, param);
 		return response.getBody();
 	}
 
